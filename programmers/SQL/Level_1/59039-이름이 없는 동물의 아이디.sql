a6a67b52ad0bb3b6fb78762946fa8649574ac9e5
kang-heesue <heesue.dev@gmail.com>
2021-09-13 21:18:58 +0900

    2021-09-13 done

---
+++ programmers/SQL/Level_1/59039-이름이 없는 동물의 아이디.sql
@@ -0,0 +1,14 @@
+/*
+ABOUT
+
+DATE: 2021-09-13
+
+URL: https://programmers.co.kr/learn/courses/30/lessons/59039
+
+COMMENT:
+NAME 컬럼이 NULL인 경우를 구한다.
+*/
+
+SELECT ANIMAL_ID
+FROM ANIMAL_INS
+WHERE NAME IS NULL;
